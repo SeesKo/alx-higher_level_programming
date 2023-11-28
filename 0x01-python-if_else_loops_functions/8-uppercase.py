31e999803e323a119ef61443a5d5ce77d5d68ae5
Siskosee <seeskosee@seeskoseesko.com>
2023-11-28 08:34:43 -0800

    printing in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    # Printing the string str in uppercase followed by a new line
+    result = ""
+    for char in str:
+        # Checking if the character is a lowercase letter
+        if 97 <= ord(char) <= 122:
+            # Convert to uppercase by subtracting 32 from the ASCII value
+            # Difference between uppercase & lowercase ASCII is 32
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print(result) # Printing the result string with a newline character
