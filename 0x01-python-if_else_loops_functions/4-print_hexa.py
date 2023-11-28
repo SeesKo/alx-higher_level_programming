744bb580b9ff023bed5de9b865cb0d76a5e725cb
Siskosee <seeskosee@seeskoseesko.com>
2023-11-28 06:23:48 -0800

    decimal-hexadecimal

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+#'{:d}' formats i as a decimal number.
+#'= 0x{:x}' formats the hexadecimal representation of i.
+
+for i in range(99):
+    print("{:d} = 0x{:x}".format(i, i))
