38e79698f3f2150def4e89ca8be28a77124fd7f8
Siskosee <seeskosee@seeskoseesko.com>
2023-11-30 08:26:42 -0800

    printing no. and list of args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,8 @@
 from sys import argv
 
 if __name__ == "__main__":
+    # Subtracting 1 coz the 1st element in
+    # argv is the script name itself.
     num_args = len(argv) - 1
 
     if num_args == 0:
@@ -9,5 +11,7 @@ if __name__ == "__main__":
         print(".")
     else:
         print("{} argument{}:".format(num_args, "s" if num_args > 1 else ""))
+        # 'enumerate' is used to get both the index (i)
+        # and the value of each argument (arg)
         for i, arg in enumerate(argv[1:], start=1):
             print("{}: {}".format(i, arg))
