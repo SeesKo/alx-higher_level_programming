bbfc73f09574a3247c6a6fde2ea24660a19ddce0
Siskosee <seeskosee@seeskoseesko.com>
2023-11-30 08:10:38 -0800

    printing no. and list of args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    num_args = len(argv) - 1
+
+    if num_args == 0:
+        print("0 arguments", end="")
+        print(".")
+    else:
+        print("{} argument{}:".format(num_args, "s" if num_args > 1 else ""))
+        for i, arg in enumerate(argv[1:], start=1):
+            print("{}: {}".format(i, arg))
