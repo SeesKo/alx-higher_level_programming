8f405bf5e6e407c5d40d2d5671456f993b0c6cc7
Siskosee <seeskosee@seeskoseesko.com>
2023-11-30 12:19:42 -0800

    magic calculation

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,12 @@
+def magic_calculation(a, b):
+    magic_module = __import__('magic_calculation_102',
+                              globals(), locals(), ['add', 'sub'], 0)
+    add, sub = magic_module.add, magic_module.sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
