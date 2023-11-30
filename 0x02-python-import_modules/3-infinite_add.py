880260be517994d8f41f5e9d9c3b01e52e183ad2
Siskosee <seeskosee@seeskoseesko.com>
2023-11-30 08:51:43 -0800

    adding up all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     # Skip the 1st element (script name) and
     # convert each argument into an integer.
     args = [int(arg) for arg in argv[1:]]
-    
+
     # Calculate the sum of all integers in arguments list
     result = sum(args)
 
