2af0883574081e3fc8b2d1ce3a1e224d0b5d3df8
Siskosee <seeskosee@seeskoseesko.com>
2023-12-02 14:28:38 -0800

    printing list in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """Prints list's integers in reverse order"""
+
+    my_list.reverse()
+
+    for i in (my_list):
+        print("{:d}".format(i))
