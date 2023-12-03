04acfd21547a04457950d6abbb4e2c96c70a0175
Siskosee <seeskosee@seeskoseesko.com>
2023-12-03 13:37:01 -0800

    printing basic python list info

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,17 @@
+#include <python.h>
+
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, i;
+	PyObject *item;
+
+	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+	size = PyList_Size(p);
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
