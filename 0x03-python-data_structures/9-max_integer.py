4eacaa7b673c1310f28cdfd2634a6d2c9d6c0c09
Siskosee <seeskosee@seeskoseesko.com>
2023-12-03 11:08:33 -0800

    finding biggest integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """Finds the biggest integer of a list"""
+
+    if not my_list:
+        return None
+
+    max_int = my_list[0]
+    for i in my_list:
+        if i > max_int:
+            max_int = i
+
+    return max_int
