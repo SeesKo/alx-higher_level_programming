546429a50969cc96bdbaf5375d13c9ebc6dac831
Siskosee <seeskosee@seeskoseesko.com>
2023-12-03 13:01:16 -0800

    check if palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,45 +1,39 @@
 #include "lists.h"
 
-/**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to the head of the linked list
- *
- * Return: 1 if the linked list is a palindrome, 0 otherwise
- */
-int is_palindrome(listint_t **head)
-{
-	if (*head == NULL || (*head)->next == NULL)
-		return (1); /* Empty list or single-node list is a palindrome */
+int is_palindrome(listint_t **head) {
+    if (*head == NULL || (*head)->next == NULL) {
+        /* An empty list or a list with a single element is considered a palindrome */
+        return 1;
+    }
 
-	listint_t *slow = *head;
-	listint_t *fast = *head;
-	listint_t *prev = NULL;
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL;
 
-	/* Move fast to the end and reverse the first half */
-	while (fast != NULL && fast->next != NULL)
-	{
-		fast = fast->next->next;
+    /* Use Floyd's Tortoise and Hare algorithm to find the middle of the list */
+    while (fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
 
-		listint_t *next = slow->next;
+        /* Reverse the first half of the list */
+        listint_t *temp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = temp;
+    }
 
-		slow->next = prev;
-		prev = slow;
-		slow = next;
-	}
+    /* If the length of the list is odd, move slow to the next node */
+    if (fast != NULL) {
+        slow = slow->next;
+    }
 
-	/* If the length is odd, skip the middle node */
-	if (fast != NULL)
-		slow = slow->next;
+    /* Compare the reversed first half with the second half */
+    while (slow != NULL) {
+        if (prev->n != slow->n) {
+            return 0;  /* Not a palindrome */
+        }
+        prev = prev->next;
+        slow = slow->next;
+    }
 
-	/* Compare the reversed first half with the second half */
-	while (prev != NULL && slow != NULL)
-	{
-		if (prev->n != slow->n)
-			return (0); /* Not a palindrome */
-
-		prev = prev->next;
-		slow = slow->next;
-	}
-
-	return (1); /* Palindrome */
+    return 1;  /* It is a palindrome */
 }
