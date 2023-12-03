75d26ca4be696a40e8950fec0ec7671868db968e
Siskosee <seeskosee@seeskoseesko.com>
2023-12-03 10:48:24 -0800

    printing integer matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers"""
+
     for row in matrix:
         for i, elem in enumerate(row):
             print("{:d}".format(elem), end="")
