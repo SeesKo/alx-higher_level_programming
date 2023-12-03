960559f06247b848ef3859cf5ab66b246f4387a5
Siskosee <seeskosee@seeskoseesko.com>
2023-12-03 10:49:22 -0800

    adding two tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """Adds 2 tuples"""
+
+    # Ensuring tuples have at least 2 elements
+    a = tuple_a[:2] + (0, 0) if len(tuple_a) < 2 else tuple_a[:2]
+    b = tuple_b[:2] + (0, 0) if len(tuple_b) < 2 else tuple_b[:2]
+
+    # Summing up the corresponding elements of the tuples
+    sum_tuple = (a[0] + b[0], a[1] + b[1])
+
+    return sum_tuple
