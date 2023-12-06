ea96c2f1c37d8b0a47b1345c2e785e12a07ea117
Siskosee <seeskosee@seeskoseesko.com>
2023-12-06 03:53:33 -0800

    updating keys/values in dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,5 +3,7 @@
 def update_dictionary(a_dictionary, key, value):
     """Replaces or adds key/value in a dictionary"""
 
+    if a_dictionary is None:
+        a_dictionary = {}
     a_dictionary[key] = value
     return a_dictionary
