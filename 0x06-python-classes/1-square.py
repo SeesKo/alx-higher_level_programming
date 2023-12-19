73033c1adea018fafa82f9c35965b395e9c1e7ea
Siskosee <seeskosee@seeskoseesko.com>
2023-12-19 03:38:48 -0800

    defining 1st square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+
 class Square:
+    """A class representing a square"""
+
     def __init__(self, size):
+        """Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square. Must be an integer.
+
+        Raises:
+            TypeError: If the size is not an integer.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
         self.__size = size
+
