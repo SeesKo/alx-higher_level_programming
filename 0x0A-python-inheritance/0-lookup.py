f4321cf537a2e2472f6e55026edeb826970fb4a9
Siskosee <seeskosee@seeskoseesko.com>
2024-01-08 01:33:12 -0800

    object attributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Defines an object-attribute-and-method-lsiting function."""
+
+
 def lookup(obj):
+    """
+    Returns a list of valid attributes for the given object.
+    """
     return dir(obj)
