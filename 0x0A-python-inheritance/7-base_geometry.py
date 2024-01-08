4cf42ec930479d4d0adcec2ec78a02c7953eaff3
Siskosee <seeskosee@seeskoseesko.com>
2024-01-08 08:06:32 -0800

    base geometry definition

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""""Defines a class representing a base geometry."""
+
+
+class BaseGeometry:
+    def area(self):
+        """Raise an Exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate the value, raising TypeError or ValueError exceptions
+        if needed.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
