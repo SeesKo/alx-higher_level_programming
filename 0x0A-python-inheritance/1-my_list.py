18281e7c63696ab8de3e76a29f6437fb5b04f66e
Siskosee <seeskosee@seeskoseesko.com>
2024-01-08 01:49:03 -0800

    inheriting a list class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class that inherits a list class."""
+
+
+class MyList(list):
+    """
+    A custom list class that inherits the built-in list class.
+    """
+    def print_sorted(self):
+        """
+        Prints the elements of the list in sorted order.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
