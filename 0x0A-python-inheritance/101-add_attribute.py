0bd09f49429361572f33417005c9ec9bf7b291a5
Siskosee <seeskosee@seeskoseesko.com>
2024-01-08 11:11:30 -0800

    adding new attribute to object

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module for adding a new attribute to an object if possible."""
+
+
+def add_attribute(obj, attribute, value):
+    """
+    Adds a new attribute to an object if possible.
+
+    Args:
+        obj: The object to which the attribute will be added.
+        attribute: The name of the new attribute.
+        value: The value of the new attribute.
+
+    Raises:
+        TypeError: If the object can't have a new attribute.
+    """
+    if "__dict__" in dir(obj):
+        setattr(obj, attribute, value)
+    else:
+        raise TypeError("can't add new attribute")
