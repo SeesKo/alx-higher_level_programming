8ae18ead83569570a576b379adf7b23a2c9b340c
Siskosee <seeskosee@seeskoseesko.com>
2024-01-09 12:12:25 -0800

    read stdin and compute metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,43 +4,38 @@ Defines a script that reads stdin line
 by line and computes metrics.
 """
 
-import signal
 import sys
+from collections import defaultdict
 
 
 def compute_metrics():
     """Computes and prints metrics based on input lines."""
     total_size = 0
-    status_codes = {}
+    status_counts = defaultdict(int)
 
     try:
-        for line_number, line in enumerate(sys.stdin, start=1):
+        for i, line in enumerate(sys.stdin, start=1):
             parts = line.split()
+
             if len(parts) >= 9:
                 status_code = parts[-2]
                 file_size = int(parts[-1])
                 total_size += file_size
+                status_counts[status_code] += 1
 
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
-                else:
-                    status_codes[status_code] = 1
+            if i % 10 == 0:
+                print(f"File size: {total_size}")
 
-            if line_number % 10 == 0:
-                print_metrics(total_size, status_codes)
+                for code in sorted(status_counts, key=int):
+                    print(f"{code}: {status_counts[code]}")
 
     except KeyboardInterrupt:
-        print_metrics(total_size, status_codes)
-        sys.exit(0)
+        pass
 
-def print_metrics(total_size, status_codes):
-    """Prints the computed metrics."""
-    print("File size: {}".format(total_size))
+    print(f"File size: {total_size}")
+    for code in sorted(status_counts, key=int):
+        print(f"{code}: {status_counts[code]}")
 
-    for code in sorted(status_codes, key=lambda x: int(x)):
-        print("{}: {}".format(code, status_codes[code]))
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, lambda signal, frame: sys.exit(0))
-
     compute_metrics()
