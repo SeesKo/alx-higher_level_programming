4391ee825181e64278a61f24de515349d6224983
Siskosee <seeskosee@seeskoseesko.com>
2024-01-09 05:41:35 -0800

    dictionary description

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,11 +7,4 @@ def class_to_json(obj):
     Returns the dictionary description with a simple
     data structure for JSON serialization.
     """
-    attributes = obj.__dict__
-    serializable_attributes = {}
-
-    for key, value in attributes.items():
-        if isinstance(value, (list, dict, str, int, bool)):
-            serializable_attributes[key] = value
-
-    return serializable_attributes
+    return obj.__dict__
