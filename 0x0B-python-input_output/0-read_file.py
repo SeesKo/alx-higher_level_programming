0cfc0d18cb48e94180cb32e4c7649c2a247ed46c
Siskosee <seeskosee@seeskoseesko.com>
2024-01-09 03:14:21 -0800

    reading a text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,8 +6,6 @@ def read_file(filename=""):
     """
     Reads a text file and prints its content to the standard output.
     """
-    try:
-        with open(filename, encoding='utf-8') as myFile:
-            print(myFile.read(), end='')
-    except FileNotFoundError:
-        print(f"Error: File '{filename}' not found.")
+    with open(filename, encoding="utf-8") as myFile:
+        for line in myFile:
+            print(line, end="")
