d90e5367a46707c185f6d9333564672410d15197
Siskosee <seeskosee@seeskoseesko.com>
2024-01-15 09:48:40 -0800

    models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,9 +38,10 @@ class Base:
         Returns:
             str: JSON string representation of list_dictionaries.
         """
-        if list_dictionaries is None or list_dictionaries == []:
+        if not list_dictionaries or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs, filename=None):
