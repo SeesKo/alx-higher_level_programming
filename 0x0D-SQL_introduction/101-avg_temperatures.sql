2358a29260b2c504a556e19de0f6c835677d90bd
Siskosee <seeskosee@seeskoseesko.com>
2024-02-13 07:02:21 -0800

    display ave temp

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,5 @@
 -- This script displays the average temperature by city, in descending order.
-USE hbtn_0c_0;
-SELECT city, AVG(temperature) AS average_temperature FROM temperatures GROUP BY city ORDER BY average_temperature DESC;
+SELECT city, AVG(temperature) AS average_temperature
+FROM temperatures
+GROUP BY city
+ORDER BY average_temperature DESC;
