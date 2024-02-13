e1fa418c12032be890fb548cca939b81bf7f6024
Siskosee <seeskosee@seeskoseesko.com>
2024-02-13 03:53:18 -0800

    creating table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- This script creates the table first_table in the specified database.
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
