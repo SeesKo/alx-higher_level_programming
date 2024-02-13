f3bc214c42a10a5441f2f030abe185b8251d2ca0
Siskosee <seeskosee@seeskoseesko.com>
2024-02-13 06:54:04 -0800

    converting to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- This script converts hbtn_0c_0 database, first_table table, and name field in first_table to UTF8.
 USE hbtn_0c_0;
 ALTER TABLE first_table
-MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
