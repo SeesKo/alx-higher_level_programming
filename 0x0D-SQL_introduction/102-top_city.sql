b5f6f0e1eea96a820a1451870fe195ec999bb508
Siskosee <seeskosee@seeskoseesko.com>
2024-02-13 07:28:02 -0800

    display top temps

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- This script displays the top 3 cities' temperatures during July and August.
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
