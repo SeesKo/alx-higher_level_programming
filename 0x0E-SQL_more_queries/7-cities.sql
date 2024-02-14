8de6c6db8bf1ad1f71f36f3fed20b0519d795a6b
Siskosee <seeskosee@seeskoseesko.com>
2024-02-14 02:33:44 -0800

    create database and table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- This script creates a database and a table on a MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
