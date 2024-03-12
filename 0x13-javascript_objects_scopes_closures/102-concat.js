7de1afd1b4d219af330ad8013be94985f45dfad7
Siskosee <seeskosee@seeskoseesko.com>
2024-03-12 15:29:40 -0700

    concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,24 +1,21 @@
 #!/usr/bin/node
 const fs = require('fs');
-// Check if correct number of arguments provided
+// Checking if correct number of arguments is provided
 if (process.argv.length !== 5) {
   console.error('Usage: node concat.js <file1> <file2> <destination>');
   process.exit(1);
 }
-// Get file paths from command line arguments
+// Getting file paths from command line arguments
 const [, , file1, file2, destination] = process.argv;
 
 try {
-  // Read content of the first file synchronously
+  // Reading content of first file synchronously
   const data1 = fs.readFileSync(file1, 'utf8');
-
-  // Read content of the second file synchronously
+  // Reading content of second file synchronously
   const data2 = fs.readFileSync(file2, 'utf8');
-
-  // Concatenate content of the two files
+  // Concatenating content of the two files
   const concatenatedContent = data1.trim() + '\n' + data2.trim() + '\n';
-
-  // Write the concatenated content to the destination file synchronously
+  // Writing concatenated content to destination file synchronously
   fs.writeFileSync(destination, concatenatedContent, 'utf8');
 } catch (err) {
   console.error(`Error: ${err}`);
