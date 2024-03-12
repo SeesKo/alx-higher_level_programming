8090f244e408aa263fc74f2c957bcfa960f9301e
Siskosee <seeskosee@seeskoseesko.com>
2024-03-12 15:21:38 -0700

    concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,35 +1,26 @@
 #!/usr/bin/node
 const fs = require('fs');
-
 // Check if correct number of arguments provided
 if (process.argv.length !== 5) {
   console.error('Usage: node concat.js <file1> <file2> <destination>');
   process.exit(1);
 }
-
 // Get file paths from command line arguments
 const [, , file1, file2, destination] = process.argv;
 
-// Read content of the first file
-fs.readFile(file1, 'utf8', (err, data1) => {
-  if (err) {
-    process.exit(1);
-  }
+try {
+  // Read content of the first file synchronously
+  const data1 = fs.readFileSync(file1, 'utf8');
 
-  // Read content of the second file
-  fs.readFile(file2, 'utf8', (err, data2) => {
-    if (err) {
-      process.exit(1);
-    }
+  // Read content of the second file synchronously
+  const data2 = fs.readFileSync(file2, 'utf8');
 
-    // Concatenate content of the two files
-    const concatenatedContent = data1.trim() + '\n' + data2.trim() + '\n';
+  // Concatenate content of the two files
+  const concatenatedContent = data1.trim() + '\n' + data2.trim() + '\n';
 
-    // Write the concatenated content to the destination file
-    fs.writeFile(destination, concatenatedContent, 'utf8', (err) => {
-      if (err) {
-        process.exit(1);
-      }
-    });
-  });
-});
+  // Write the concatenated content to the destination file synchronously
+  fs.writeFileSync(destination, concatenatedContent, 'utf8');
+} catch (err) {
+  console.error(`Error: ${err}`);
+  process.exit(1);
+}
