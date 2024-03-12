6e6f22fccfc94f817d5d19f973d3c1cadc099820
Siskosee <seeskosee@seeskoseesko.com>
2024-03-12 14:46:16 -0700

    concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,18 +1,38 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 const fs = require('fs');
-// Check if correct number of arguments are provided
+
+// Check if correct number of arguments is provided
 if (process.argv.length !== 5) {
-    console.error("Usage: ./102-concat.js <file1> <file2> <output>");
-    process.exit(1);
+  console.error('Usage: node concat.js <file1> <file2> <destination>');
+  process.exit(1);
 }
-const [, , sourceFile1, sourceFile2, destinationFile] = process.argv;
-// Read content of source files
-const content1 = fs.readFileSync(sourceFile1, 'utf8').trim();
-const content2 = fs.readFileSync(sourceFile2, 'utf8').trim();
-// Concatenate content
-const concatContent = content1 + '\n' + content2;
-// Print concatenated content
-console.log(concatContent);
-// Write concatenated content to destination file
-fs.writeFileSync(destinationFile, concatContent);
-process.exit(0);
+
+const [, , file1, file2, destination] = process.argv;
+
+// Read the content of file1
+fs.readFile(file1, 'utf8', (err, data1) => {
+  if (err) {
+    console.error(`Error reading file ${file1}:`, err);
+    process.exit(1);
+  }
+
+  // Read the content of file2
+  fs.readFile(file2, 'utf8', (err, data2) => {
+    if (err) {
+      console.error(`Error reading file ${file2}:`, err);
+      process.exit(1);
+    }
+
+    // Concatenate data1 and data2
+    const concatenatedData = data1.trim() + '\n' + data2.trim();
+
+    // Write concatenated content to the destination file
+    fs.writeFile(destination, concatenatedData, (err) => {
+      if (err) {
+        console.error(`Error writing to file ${destination}:`, err);
+        process.exit(1);
+      }
+      console.log(concatenatedData);
+    });
+  });
+});
