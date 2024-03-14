6774067e873866d3c4be1a22fbf78e66eb1c7e42
Siskosee <seeskosee@seeskoseesko.com>
2024-03-14 16:29:08 -0700

    take args, display values

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Retrieving command line arguments
+    username, password, database, state_name = sys.argv[1:5]
+    # Connecting to MySQL database
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database)
+    # Creating cursor object
+    cursor = conn.cursor()
+    # Executing SQL query to select states based on user input
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+    # Fetching all rows
+    rows = cursor.fetchall()
+    # Displaying results
+    for row in rows:
+        print(row)
+    # Closing cursor and database connection
+    cursor.close()
+    conn.close()
