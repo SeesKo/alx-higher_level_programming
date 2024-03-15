381cd460a9524e59e2bc4389e6bca55c727a63c2
Siskosee <seeskosee@seeskoseesko.com>
2024-03-14 23:20:38 -0700

    list all state's cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,30 +2,26 @@
 """
 Script that lists all cities of a given state from the database hbtn_0e_4_usa.
 """
-import sys
-import MySQLdb
 
 if __name__ == "__main__":
-    # Retrieving command line arguments
-    username, password, database, state_name = sys.argv[1:5]
-    # Connecting to MySQL database
-    conn = MySQLdb.connect(user=username, passwd=password, db=database)
-    cursor = conn.cursor()
-    state_name = sys.argv[4]
-    # Execute SQL query to select cities of the given state
-    cursor.execute("SELECT cities.id, cities.name, states.name \
-                    FROM cities \
-                    JOIN states ON cities.state_id = states.id \
-                    ORDER BY states.id")
-    # Fetching all rows
-    rows = cursor.fetchall()
-    # Extracting city names for the given state
-    cities_list = []
+    import sys
+    import MySQLdb
+
+    serv = MySQLdb.connect(host="localhost",  port=3306,
+                           user=sys.argv[1], password=sys.argv[2],
+                           database=sys.argv[3])
+
+    newList = []
+    c = serv.cursor()
+    stateName = sys.argv[4]
+    c.execute("SELECT c.id, c.name, s.name\
+                    FROM cities AS c\
+                    JOIN states AS s \
+                    ON c.state_id=s.id ORDER BY s.id;")
+    rows = c.fetchall()
     for row in rows:
-        if row[2] == state_name:
-            cities_list.append(row[1])
-    # Displaying results
-    print(', '.join(cities))
-    # Closing cursor and database connection
-    cursor.close()
-    conn.close()
+        if row[2] == stateName:
+            newList.append(row[1])
+    print(', '.join(newList))
+    c.close()
+    serv.close()
