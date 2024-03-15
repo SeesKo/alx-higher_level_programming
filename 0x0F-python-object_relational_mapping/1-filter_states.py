875e2927d585f03fe8fa96a9fe4bab38449a9a71
Siskosee <seeskosee@seeskoseesko.com>
2024-03-14 21:35:32 -0700

    starting with given letter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-"""Script that lists all states with a name starting
+"""
+Script that lists all states with a name starting
 with N from the database hbtn_0e_0_usa."""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
+    # Retrieving command line arguments
+    username, password, database = sys.argv[1:3]
 
-    serv = MySQLdb.connect(host="localhost",  port=3306,
-                           user=sys.argv[1], password=sys.argv[2],
-                           database=sys.argv[3])
+    # Connecting to MySQL database
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database)
 
-    c = serv.cursor()
-    c.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = c.fetchall()
+    # Creating cursor object
+    cursor = conn.cursor()
+
+    # Executing SQL query to select states with name starting with 'N'
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Fetching all rows
+    rows = cursor.fetchall()
+
+    # Displaying results
     for row in rows:
-        if row[1][0] == 'N':
-            print(row)
-    c.close()
-    serv.close()
+        print(row)
+
+    # Closing cursor and database connection
+    cursor.close()
+    conn.close()
