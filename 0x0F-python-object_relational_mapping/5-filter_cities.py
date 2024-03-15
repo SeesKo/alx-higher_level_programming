0b5db994bb1b79ed733f05dd8c72d9a2b893c429
Siskosee <seeskosee@seeskoseesko.com>
2024-03-14 23:38:36 -0700

    list all state's cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,23 +6,20 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Extracting command line arguments
-    username, password, database, state_name = sys.argv[1:5]
 
-    # Connecting to MySQL database
+    username, password, database, state_name = sys.argv[1:5]
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=username, passwd=password,
                            database=database)
 
     # Creating cursor object
     cursor = conn.cursor()
-
     # Executing SQL query to select cities of the given state
-    cursor.execute("SELECT c.name\
-                    FROM cities AS c\
-                    JOIN states AS s ON c.state_id = s.id\
-                    WHERE s.name = %s\
-                    ORDER BY c.id ASC", (state_name,))
+    cursor.execute("SELECT c.name"
+                   "FROM cities AS c"
+                   "JOIN states AS s ON c.state_id = s.id"
+                   "WHERE s.name = %s"
+                   "ORDER BY c.id ASC", (state_name,))
 
     # Fetching all rows
     rows = cursor.fetchall()
@@ -34,7 +31,6 @@ if __name__ == "__main__":
 
     # Displaying results
     print(', '.join(cities))
-
     # Closing cursor and database connection
     cursor.close()
     conn.close()
