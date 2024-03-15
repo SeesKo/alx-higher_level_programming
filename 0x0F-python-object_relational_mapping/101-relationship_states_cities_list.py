bae425d36d0d6d68a080eed0350e8097fc9867ca
Siskosee <seeskosee@seeskoseesko.com>
2024-03-15 08:54:08 -0700

    state obj with matching city obj

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""
-Lists all State objects, and corresponding City objects,
-contained in the database hbtn_0e_101_usa.
+""" prints the State object with the name passed as argument from the database
 """
 import sys
-from sqlalchemy import create_engine
 from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import relationship
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
