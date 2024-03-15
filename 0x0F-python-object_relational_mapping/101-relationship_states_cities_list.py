71bed2b7b156c7313a1a8b7970241c50663d4ceb
Siskosee <seeskosee@seeskoseesko.com>
2024-03-15 08:51:49 -0700

    state obj with matching city obj

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -11,25 +11,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import relationship
 
 if __name__ == "__main__":
-    username, password, database = sys.argv[1:4]
-
-    # Creating the engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(username, password, database),
-                           pool_pre_ping=True)
-
-    # Creating a session to interact with the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Querying all State objects
-    states = session.query(State).order_by(State.id).all()
-
-    # Printing State and corresponding City objects
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
-
-    # Closing the session
-    session.close()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
+        for city_ins in instance.cities:
+            print("    ", end="")
+            print(city_ins.id, city_ins.name, sep=": ")
