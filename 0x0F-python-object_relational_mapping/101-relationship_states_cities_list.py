c300666a12ba6affe6c84e00d299afe9d256d580
Siskosee <seeskosee@seeskoseesko.com>
2024-03-15 15:11:00 -0700

    state obj with matching city obj

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,33 +1,41 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects, and corresponding City
-objects, contained in the database hbtn_0e_101_usa.
+Script that lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa
 """
+
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
-if __name__ == "__main__":
-    # Create engine to connect to the database
+
+def list_states_cities(username, password, db_name):
+    """
+    List all State objects and corresponding City objects
+    """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           .format(username, password, db_name),
                            pool_pre_ping=True)
 
-    # Create a session to interact with the database
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query database to get all State objects and
-    # their corresponding City objects
     states = session.query(State).order_by(State.id).all()
 
-    # Print the results
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("\t{}: {}".format(city.id, city.name))
 
-    # Close the session
     session.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    list_states_cities(sys.argv[1], sys.argv[2], sys.argv[3])
