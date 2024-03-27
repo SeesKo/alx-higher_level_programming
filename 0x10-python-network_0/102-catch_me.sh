81af6a873d922c92f819546697816c76f66e618b
Siskosee <seeskosee@seeskoseesko.com>
2024-03-27 07:51:29 -0700

    request to url endpoint

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes request to 0.0.0.0:5000/catch_me that causes the server to respond with a message
-curl -s -X PUT 0.0.0.0:5000/catch_me -L -d "user_id=98" -H "Origin: HolbertonSchool"
+curl -s -X PUT 0.0.0.0:5000/catch_me -L -d "user_id=98" -H "Origin: OriginPoint"
