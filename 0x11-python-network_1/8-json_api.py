0304a4027b2045e723fc0ef543246982cbaab619
Siskosee <seeskosee@seeskoseesko.com>
2024-03-28 02:15:51 -0700

    send post request to url

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that takes in a letter and sends a POST
+request to URL with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    payload = {'q': q}
+
+    try:
+        response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
