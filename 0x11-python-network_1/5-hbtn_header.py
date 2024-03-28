00d8e76ef9047d90783f96860c3543c44f356a25
Siskosee <seeskosee@seeskoseesko.com>
2024-03-28 02:13:48 -0700

    send request, show id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends it a request, then displays
+the value of the variable X-Request-Id in the response header.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    request_id = response.headers.get('X-Request-Id')
+    print(request_id)
