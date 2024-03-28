a0a264adb94a25903e8c2d654c99577924e6efae
Siskosee <seeskosee@seeskoseesko.com>
2024-03-28 03:04:43 -0700

    list commits, newest to oldest

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script listing commits (from the most recent to oldest) of
+the repository specified by the owner and repository name.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
+    response = requests.get(url)
+
+    commits = response.json()[:10]  # Get the first 10 commits
+
+    for commit in commits:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
