f5d3483fbfd3037dfdd87e742b5f53f5c3e7786e
Siskosee <seeskosee@seeskoseesko.com>
2024-03-28 02:15:08 -0700

    send request, show response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that takes in URL, sends it request, then displays response's body.
+If HTTP status code is greater than or equal to 400, it prints 
+"Error code:" followed by the value of the HTTP status code.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
